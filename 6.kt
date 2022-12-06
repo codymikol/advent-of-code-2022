feb79496d75d1a47054685dba5fea4fa88b8e035
Cody Mikol <codymikol@gmail.com>
2022-12-06 00:49:38 -0500

    feat: day 6 update

--- 6.kt
+++ 6.kt
@@ -3,7 +3,7 @@ import java.nio.file.Paths
 
 
 fun main() {
-    val input = File(Paths.get("/home/cody/dev/src/advent-of-code-2022/input/6.txt").toUri()).readText()
+    val input = File(Paths.get("./input/6.txt").toUri()).readText()
     listOf(4, 14).forEach { println("The start of message for for count $it is: ${findStartOfMessage(it, input)}") }
 }
 
